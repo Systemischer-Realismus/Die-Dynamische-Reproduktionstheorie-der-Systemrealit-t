b27d16b1a5a9a7fe831064aa4b2382d3a01aa669
Systemischer-Realismus <Systemischer.Realismus@outlook.de>
2026-02-27 16:18:29 +0100

    Update earth.js

--- assets/js/earth.js
+++ assets/js/earth.js
@@ -10,9 +10,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const totalSteps = 113;
 
   // Berechnung der Rotations- und Kippwinkel
-  const rotation = pageIndex * (360 / totalSteps);
+  //const rotation = pageIndex * (360 / totalSteps);
   const tilt = 180 - pageIndex * (180 / totalSteps);
-
+  const rotation = 0;
   // Transformation anwenden
-  earth.style.transform = `rotateZ(${rotation}deg) rotateX(${tilt}deg)`;
-});
+  earth.style.transform = ` rotateX(${tilt}deg)`;//rotateZ(${rotation}deg)
+  });
